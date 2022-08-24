d8ada16377563b7f876fd376b06ffa90a902263e
seigi1 <seigi1224@gmail.com>
2022-08-24 20:14:04 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,62 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :new, :create, :show, :destroy]
+
+
+  def index
+    @prototype = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+      if @prototype.save
+        redirect_to root_path
+      else
+        render new_prototype_path
+      end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+     if prototype.update(prototype_params)
+        redirect_to prototype_path(prototype.user)
+     else
+        render :edit
+     end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    if  prototype.destroy
+        redirect_to root_path
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+
+end
